98cef458196d503334c636edd3b9da2f95b4b800
Mario Visic <mario@mariovisic.com>
2024-06-29 11:40:39 +1000

    Add tab autocomplete support from luasnip

--- nvim.init.lua
+++ nvim.init.lua
@@ -44,12 +44,50 @@ local cmp_spec = {
   },
   config = function()
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
     cmp.setup({
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }
       }),
-      mapping = cmp.mapping.preset.insert({}),
+      mapping = cmp.mapping.preset.insert({
+
+       ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                if luasnip.expandable() then
+                    luasnip.expand()
+                else
+                    cmp.confirm({
+                        select = true,
+                    })
+                end
+            else
+                fallback()
+            end
+        end),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+
       snippet = {
         expand = function(args)
           require('luasnip').lsp_expand(args.body)
