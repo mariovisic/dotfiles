7f3f8955684b3e6a78d63bd54f15c0aa467ea498
Mario Visic <mario@mariovisic.com>
2024-07-09 15:44:29 +1000

    Add command copy+paste support to neovide

--- nvim.init.lua
+++ nvim.init.lua
@@ -122,4 +122,17 @@ if vim.g.neovide then
   vim.cmd[[colorscheme tokyonight]]
   -- Disable the neovide cursor animations as they're distrating
   vim.g.neovide_cursor_animation_length = 0
+
+  -- Allow clipboard copy paste in neovide
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 end
