18a64e188d00fde178204f158333a99eb7b24290
Mario Visic <mario@mariovisic.com>
2024-07-14 22:15:54 +1000

    Neovim: add back in indent guide comment plugin

--- nvim.init.lua
+++ nvim.init.lua
@@ -80,14 +80,27 @@ local mini_spec = {
     -- Show a status line down the bottom
     require('mini.statusline').setup()
 
+    -- Show indentation scope for current line
+    require('mini.indentscope').setup({
+      draw = {
+        -- Use a little delay with no animations to make it seem seemless
+        delay = 30,
+        animation = require('mini.indentscope').gen_animation.none(),
+      },
+      options = {
+        indent_at_cursor = false,
+      },
+      symbol = '|',
+    })
+
     -- Comment out lines with \c + vector
-    require('mini.comment').setup{
+    require('mini.comment').setup({
       mappings = {
         comment = '<leader>c',
         comment_visual = '<leader>c',
         comment_line = '<leader>cc',
       }
-    }
+    })
   end,
 }
 
