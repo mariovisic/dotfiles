78a597f90c740178722bf87c143ccfdfa71f8b52
Mario Visic <mario@mariovisic.com>
2024-07-19 21:06:11 +1000

    Neovim: map ctrl+c to esc to fix a bug

--- nvim.init.lua
+++ nvim.init.lua
@@ -23,6 +23,10 @@ vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-p>", { noremap = true, silent = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- I use ctrl+c instead of escape, but ctrl+c doesn't trigger some events which
+-- are needed for LSP, let's map ctrl+c to match escape to get this behaviour
+vim.keymap.set({'i', 'n', 'v'}, '<C-C>', '<esc>')
+
 -- <leader>n opens nvim-tree
 vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', {
   noremap = true
