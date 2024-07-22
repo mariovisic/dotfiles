584ebdd9eaf55881d06d78928c33c00692aa2a4e
Mario Visic <mario@mariovisic.com>
2024-07-23 09:48:37 +1000

    Add lua laugage server

--- nvim.init.lua
+++ nvim.init.lua
@@ -227,8 +227,39 @@ lsp_zero.on_attach(function(client, bufnr)
 end)
 
 -- Setup specific language servers (manually installed) :)
+
+-- Typescript
 require('lspconfig').tsserver.setup({})
+
+-- Ruby
 require('lspconfig').sorbet.setup({})
+
+-- Lua
+require('lspconfig').lua_ls.setup({
+  settings = {
+    Lua = {}
+  },
+  on_init = function(client)
+    local uv = vim.uv or vim.loop
+    local path = client.workspace_folders[1].name
+
+    -- Don't do anything if there is a project local config
+    if uv.fs_stat(path .. '/.luarc.json')
+      or uv.fs_stat(path .. '/.luarc.jsonc')
+    then
+      return
+    end
+
+    -- Apply neovim specific settings
+    local lua_opts = lsp_zero.nvim_lua_ls()
+
+    client.config.settings.Lua = vim.tbl_deep_extend(
+      'force',
+      client.config.settings.Lua,
+      lua_opts.settings.Lua
+    )
+  end,
+})
 
 -- Setup tab autocomplete for lsp
 lsp_zero.omnifunc.setup({
