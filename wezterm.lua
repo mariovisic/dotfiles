ab8a850ebd4b82f8c423fdba041cf9e13b3a339e
Mario Visic <mario@mariovisic.com>
2024-09-17 10:23:15 +1000

    Add wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,81 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+local act = wezterm.action
+
+local config = {}
+
+config.keys = {
+  -- Clear the scrollback and viewport, then send CTRL-L to redraw prompt
+  {
+    key = "k",
+    mods = "CMD",
+    action = act.Multiple({ act.ClearScrollback("ScrollbackAndViewport"), act.SendKey({ key = "L", mods = "CTRL" }) }),
+  },
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
+
+config.font_size = 18
+
+-- START TAB STYLING !!!
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local edge_background = "#0b0022"
+  local background = "#1b1032"
+  local foreground = "#808080"
+
+  if tab.is_active then
+    background = "#2b2042"
+    foreground = "#c0c0c0"
+  elseif hover then
+    background = "#3b3052"
+    foreground = "#909090"
+  end
+
+  local edge_foreground = background
+
+  local title = tab_title(tab)
+
+  -- ensure that the titles fit in the available space,
+  -- and that we have room for the edges.
+  title = wezterm.truncate_right(title, max_width - 2)
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
+-- END Tab Styling !!!
+
+return config
