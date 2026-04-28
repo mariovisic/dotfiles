9b4e6d941ffe814c57094b21b6f27122d61b4338
Mario Lesniak <mario@mariovisic.com>
2026-04-29 00:01:20 +1000

    Open either sorbet OR ruby_lsp depending if a file is typed or not!

--- nvim/lua/config/language_server_protocol.lua
+++ nvim/lua/config/language_server_protocol.lua
@@ -4,6 +4,20 @@
 -- Custom LSP server configurations --
 -- ================================ --
 
+-- Check if the current buffer is typed with sorbet containing either
+-- typed: true OR typed: strict
+local function typed_with_sorbet(bufnr)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 10, false)
+
+  for _, line in ipairs(lines) do
+    if line:match("#%s*typed:%s*true") or line:match("#%s*typed:%s*strict") then
+      return true
+    end
+  end
+
+  return false
+end
+
 -- Setup lua to be able to work on neovim config!
 vim.lsp.config("lua_ls", {
   settings = {
@@ -22,11 +36,45 @@ vim.lsp.config("lua_ls", {
   end,
 })
 
--- Hack to fix sorbet CWD ... only works on 2nd buffer annoyingly!
 vim.lsp.config("sorbet", {
+  -- Neovim built in LSP launches sorbet from the editors current directory,
+  -- which can be different to the projects root diretory. Sorbet can't
+  -- reference other classes when this happens and becomes useless.
+  --
+  -- This hack sets the current working directory for sorbet to match root_dir!
   reuse_client = function(client, config)
     config.cmd_cwd = config.root_dir
-    return client.config.cmd_cwd == config.cmd_cwd
+    return client.config.cmd_cwd == config.cmd_cwd and client.config.name == config.name
+  end,
+
+  -- Only load sorbet for ruby files that have typing enabled!
+  root_dir = function(bufnr, on_dir)
+    if typed_with_sorbet(bufnr) then
+      on_dir(vim.fs.root(bufnr, vim.lsp.config.sorbet.root_markers))
+    end
+  end,
+})
+
+vim.lsp.config("ruby_lsp", {
+  cmd_env = {
+    RUBY_LSP_BYPASS_TYPECHECKER = "yes",
+  },
+
+  -- This function is set in nvim-lspconfig with a bug.
+  -- It only checks `cmd_cwd` and no other attributes like the name of the LSP.
+  -- When this is set for only one language server for ruby then there are no issues.
+  --
+  -- When using sorbet + ruby-lsp together we end up re-using the sorbet client instead of ruby_lsp and vice-versa!
+  reuse_client = function(client, config)
+    config.cmd_cwd = config.root_dir
+    return client.config.cmd_cwd == config.cmd_cwd and client.config.name == config.name
+  end,
+
+  -- Only load ruby_lsp for ruby files that are not typed with sorbet!
+  root_dir = function(bufnr, on_dir)
+    if not typed_with_sorbet(bufnr) then
+      on_dir(vim.fs.root(bufnr, vim.lsp.config.ruby_lsp.root_markers))
+    end
   end,
 })
 
